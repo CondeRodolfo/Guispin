10a69529f5ff2a01e43a58654fe331cc9050bc2a
Rodolfo Conde <rodolfo.mconde@gmail.com>
2025-08-18 21:10:04 +0800

    Added EF with postgresql provider

---
+++ Guispin.Server/Controllers/PingController.cs
@@ -0,0 +1,21 @@
+﻿using Guispin.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guispin.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PingController : Controller
+    {
+        [HttpGet]
+        [Route("get")]
+        public IActionResult Get()
+        {
+            Player player = new Player();
+            player.Level = 100;
+            player.Name = "Rod";
+
+            return Ok(new { Message = player });
+        }
+    }
+}
---
+++ Guispin.Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Guispin.Shared.Models;
+
+namespace Guispin.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) { }
+
+        // Example/Test table
+        public DbSet<Player>? Players { get; set; } = null;
+    }
+}
